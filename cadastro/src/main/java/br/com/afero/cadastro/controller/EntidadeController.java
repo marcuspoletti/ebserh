97b19c5b936fdac7b7cdaeae1ff61bcd5e602d1e
marcuspoletti <marcusvpoletti@gmail.com>
2025-07-11 08:40:18 -0300

    Teste2

---
+++ cadastro/src/main/java/br/com/afero/cadastro/controller/EntidadeController.java
@@ -0,0 +1,26 @@
+package br.com.afero.cadastro.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+@RestController
+public class EntidadeController {
+	
+    
+    @GetMapping("/")
+	public ModelAndView index() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("home/index");
+    	return mv;
+	}
+	
+
+	
+
+}
