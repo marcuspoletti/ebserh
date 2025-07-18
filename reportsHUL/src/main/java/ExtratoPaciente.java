c5b0465e00d121203cb43f4e2149805b5fe6d93f
marcuspoletti <marcusvpoletti@gmail.com>
2025-07-18 16:13:55 -0300

    Começo dos Códigos

---
+++ reportsHUL/src/main/java/ExtratoPaciente.java
@@ -0,0 +1,36 @@
+
+public class ExtratoPaciente {
+	
+	private String grauInstrucao;
+	private String cor;
+	private String estado_civil;
+	private String cidPrimario;
+	private String cid;
+	private String nomeEspecialidade;
+	private String unidadeFuncional;
+	private String unidadeAnterior;
+	private String tpMovimentoInternacao;
+	private Integer prontuario;
+	private String nome;
+	private String sexo;
+	
+	
+	private String dtNascimento;
+	private String dtLancamento;
+	private String dtInternacao;
+	private String dtAltaMedica;
+	private Integer motAlta;
+	private String dtHrLancamento;
+	private String ltoLtoId;
+	
+	
+	private Integer intSeq;
+	private Integer seq;
+	private String dtHrInternacao;
+	
+	private Integer intSeq;
+	private Integer seq;
+	private String dtHrInternacao;
+}
+
+
